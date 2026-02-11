94b582e888485624c5079f9cf3fb6ffd1d9abd7a
Hugo Kajdan <hugo@kajdan.fr>
2026-02-11 15:07:17 +0100

    fixed menu items at to full height and removed buttons implementation

--- apps/web/src/lib/components/layout/NavbarDesktop.svelte
+++ apps/web/src/lib/components/layout/NavbarDesktop.svelte
@@ -78,11 +78,11 @@
     </div>
 
     <!-- Menu principal -->
-    <ul class="hidden lg:flex items-center">
+    <ul class="hidden lg:flex items-center h-18">
       {#each props.navbar?.columns as column (column._key || `col-${column._type}-${column.title}`)}
-        <li class="group nth-[n+5]:bg-primary-500 px-2 py-6 last:pr-4 last:rounded-r-xl">
+        <li class="group nth-[n+5]:bg-primary-500 px-4 h-full" >
           {#if column._type === 'navbarColumn'}
-            <button class="flex items-center px-3 text-neutral-700 hover:text-primary-600 h-full transition-colors font-medium bg-white">
+            <button class="flex items-center text-neutral-700 group-hover:text-primary-500 h-full transition-colors ">
               {column.title}
               <svg class="ml-1 h-4 w-4" fill="currentColor" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -120,7 +120,7 @@
           {:else}
             <a 
               href={resolveSanityUrl(column.url)}
-              class="px-3 text-white bg-blue rounded-full h-full hover:bg-primary-600 transition-colors font-medium"
+              class="flex items-center text-white transition-colors h-full"
             >
               {column.name}
             </a>
@@ -128,25 +128,10 @@
         </li>
       {/each}
       
-      <!-- Buttons -->
-      {#if props.navbar?.buttons && props.navbar.buttons.length > 0}
-        <li class="flex items-center h-full space-x-2 ml-6">
-          {#each props.navbar.buttons as button (button._key || `btn-${button.label}`)}
-            <a 
-              href={button.url?.href || '#'} 
-              class="px-4 py-2 h-full border border-primary-600 text-primary-600 bg-primary-500 hover:text-white transition-colors text-sm font-medium"
-            >
-              {button.label}
-            </a>
-          {/each}
-        </li>
-      {/if}
-      
-      <!-- Contact Button - scrolls to #contact section -->
       <li class="flex items-center h-full">
         <a 
           href="#contact" 
-          class="px-4 py-6 h-full bg-primary-500 text-white"
+          class="px-4 py-6 bg-primary-500 text-white"
           aria-label="Contact"
         >
           Contact
